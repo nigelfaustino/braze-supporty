55056a3494ff1b51c46d09168b2e6f7c3f05d2b3
Nigel Faustino <nigel.faustino@braze.com>
2025-01-07 10:08:57 -0800

    Initial Commit

---
+++ SupportyTests/SupportyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SupportyTests.swift
+//  SupportyTests
+//
+//  Created by Nigel Faustino on 1/7/25.
+//
+
+import Testing
+@testable import Supporty
+
+struct SupportyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
